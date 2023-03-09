d753b46bb6394dec8ea07bc361cabdaa9611d21a
Tyler Sweeting <sweetingwt@gmail.com>
2023-03-09 10:59:52 -0500

    working on module

---
+++ static/js/logicStep1.js
@@ -0,0 +1,8 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite": satelliteStreets
+  };
